3419e7a8419cd896a11b6c43d8b32cfa955b3198
RemxFr <epourexisterpro@gmail.com>
2023-10-12 11:01:30 +0200

    Initial commit

---
+++ serverWs/src/main/java/com/pixelWar/serverWs/ServerWsApplication.java
@@ -0,0 +1,13 @@
+package com.pixelWar.serverWs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerWsApplication.class, args);
+	}
+
+}
---
+++ serverWs/src/test/java/com/pixelWar/serverWs/ServerWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelWar.serverWs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
